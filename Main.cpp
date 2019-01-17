c06f2ff9252eac25addc5a466632cc5a0f647ea9
Klotzkopf <m.famao@web.de>
2019-01-17 11:14:02 +0100

    -

--- Main.cpp
+++ Main.cpp
@@ -6,6 +6,120 @@
 #include "Trie2.h"
 #include "iostream"
 
+bool testTrieInsertAndFind();
+bool testBeginAndEnd();
+bool testUpperBound();
+bool testIncrementInterator();
+bool testErase();
+bool testLowerBound();
+bool testClear();
+
+
+//------------ start here ----------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------
+using namespace std;
+
+int main() {
+
+    Trie2<std::string, char> trie = Trie2<std::string, char>();
+
+    int count;
+    cout << "Wie viele Eintraege wollem Sie in den Trie hinzufuegen?" << endl;
+    cin >> count;
+
+    //fill in trie
+    for (int i = 0; i < count; ++i) {
+        cout << "Erstelle Eintrag Nummer " << i << endl;
+        cout << "Wie lautet der Key " << i << "?" << endl;
+        std::basic_string<char> key;
+        cin >> key;
+
+        cout << "Wie lautet der Value " << i << "?" << endl;
+        std::string value;
+        cin >> value;
+
+        std::pair<std::basic_string<char>, std::string> newPair = std::make_pair(key, value);
+        trie.insert(newPair);
+    }
+    cout << "Eintraege wurden hinzugefuegt" << endl;
+
+    //search student
+    cout << "Suche einen Eintrag ueber seinen Key!" << endl;
+    std::basic_string<char> searchedKey;
+    cin >> searchedKey;
+    Trie2<std::string, char>::PostorderTreeTraversal2 searchIt = trie.find(searchedKey);
+    if (searchIt != trie.end()) {
+        cout << "Eintrag mit value: " << (*searchIt) << endl;
+    } else {
+        cout << "Es wurden keine Eintraege mit dem Key " << searchedKey << " gefunden" << endl;
+    }
+
+    cout << "Moechten Sie Ihren Trie anzeigen lassen? (false=0/true=1)" << endl;
+    bool showTree;
+    cin >> showTree;
+
+    if (showTree) {
+        trie.printTree();
+    }
+
+    cout << endl << "Moechten Sie einen Eintrag im Trie entfernen? (false=0/true=1)" << endl;
+    bool iserase;
+    cin >> iserase;
+
+    if (iserase) {
+        cout << "Welchen Key wollen sie loeschen? Wenn der Key nicht vorhanden ist passiert nichts." << endl;
+        cin >> searchedKey;
+        trie.erase(searchedKey);
+        cout << "Ihr Baum sieht nun so aus: " << endl;
+        trie.printTree();
+        cout << endl << "Eingabe beendet." << endl;
+    } else {
+        cout << endl << "Eingabe beendet." << endl;
+    }
+
+    cout << endl << endl;
+//TestMethods for Trie
+    if (testTrieInsertAndFind()) {
+        std::cout << "Test Insert() and Find() passed \n";
+    } else {
+        std::cout << "Test Insert() and Find() failed.\n";
+    }
+    if (testBeginAndEnd()) {
+        std::cout << "Test Begin() and End() passed\n";
+    } else {
+        std::cout << "Test Begin() and End() failed.\n";
+    }
+    if (testUpperBound()) {
+        std::cout << "Test Upper_Bound() passed\n";
+    } else {
+        std::cout << "Test Upper_Bound() failed.\n";
+    }
+    if (testIncrementInterator()) {
+        std::cout << "Test testIncrementInterator() passed\n";
+    } else {
+        std::cout << "Test testIncrementInterator() failed.\n";
+    }
+    if (testErase()) {
+        std::cout << "Test testErase() passed\n";
+    } else {
+        std::cout << "Test testErase() failed.\n";
+    }
+    if (testLowerBound()) {
+        std::cout << "Test testLowerBound() passed\n";
+    } else {
+        std::cout << "Test testLowerBound0() failed.\n";
+    }
+    if (testClear()) {
+        std::cout << "Test testClear() passed\n";
+    } else {
+        std::cout << "Test testClear() failed.\n";
+    }
+
+
+    return 0;
+}
+
+
 bool testTrieInsertAndFind() {
     Trie2<std::string, char> testTrie = Trie2<std::string, char>();
     Trie2<std::string, char>::PostorderTreeTraversal2 testIt01 = testTrie.insert(std::make_pair("Katze", "cat"));
@@ -91,15 +205,15 @@ bool testIncrementInterator() {
     Trie2<std::string, char>::PostorderTreeTraversal2 testIt01 = testTrie.begin();
     allPassed = (*testIt01) == "anker";
     ++testIt01;
-    allPassed = (*testIt01) == "ball";
+    allPassed = allPassed ? (*testIt01) == "ball" : allPassed;
     ++testIt01;
-    allPassed = (*testIt01) == "boy";
+    allPassed = allPassed ? (*testIt01) == "boy" : allPassed;
     ++testIt01;
-    allPassed = (*testIt01) == "sure";
+    allPassed = allPassed ? (*testIt01) == "sure" : allPassed;
     ++testIt01;
-    allPassed = (*testIt01) == "soup";
+    allPassed = allPassed ? (*testIt01) == "soup" : allPassed;
     ++testIt01;
-    allPassed = testIt01 == testTrie.end();
+    allPassed = allPassed ? testIt01 == testTrie.end() : allPassed;
 
     return allPassed;
 }
@@ -167,108 +281,4 @@ bool testClear() {
 
     testTrie.clear();
     return testTrie.empty();
-}
-
-//------------ start here ----------------------------------------------------------------------------------------------
-//----------------------------------------------------------------------------------------------------------------------
-using namespace std;
-
-int main() {
-
-    Trie2<std::string, char> trie = Trie2<std::string, char>();
-
-    int count;
-    cout << "Wie viele Eintraege wollem Sie in den Trie hinzufuegen?" << endl;
-    cin >> count;
-
-    //fill in trie
-    for (int i = 0; i < count; ++i) {
-        cout << "Erstelle Eintrag Nummer " << i << endl;
-        cout << "Wie lautet der Key " << i << "?" << endl;
-        std::basic_string<char> key;
-        cin >> key;
-
-        cout << "Wie lautet der Value " << i << "?" << endl;
-        std::string value;
-        cin >> value;
-
-        std::pair<std::basic_string<char>, std::string> newPair = std::make_pair(key, value);
-        trie.insert(newPair);
-    }
-    cout << "Eintraege wurden hinzugefuegt" << endl;
-
-    //search student
-    cout << "Suche einen Eintrag ueber seinen Key!" << endl;
-    std::basic_string<char> searchedKey;
-    cin >> searchedKey;
-    Trie2<std::string, char>::PostorderTreeTraversal2 searchIt = trie.find(searchedKey);
-    if (searchIt != trie.end()) {
-        cout << "Eintrag mit value: " <<  (*searchIt) << endl;
-    } else {
-        cout << "Es wurden keine Eintraege mit dem Key " << searchedKey << " gefunden" << endl;
-    }
-
-    cout << "Moechten Sie Ihren Trie anzeigen lassen? (false=0/true=1)" << endl;
-    bool showTree;
-    cin >> showTree;
-
-    if (showTree) {
-        trie.printTree();
-    }
-
-    cout << endl<< "Moechten Sie einen Eintrag im Trie entfernen? (false=0/true=1)" << endl;
-    bool iserase;
-    cin >> iserase;
-
-    if (iserase) {
-        cout << "Welchen Key wollen sie loeschen? Wenn der Key nicht vorhanden ist passiert nichts." << endl;
-        cin >> searchedKey;
-        trie.erase(searchedKey);
-        cout << "Ihr Baum sieht nun so aus: " << endl;
-        trie.printTree();
-        cout << endl << "Eingabe beendet." << endl;
-    } else {
-        cout << endl << "Eingabe beendet." << endl;
-    }
-
-    cout << endl << endl;
-//TestMethods for Trie
-    if (testTrieInsertAndFind()) {
-        std::cout << "Test Insert() and Find() passed \n";
-    } else {
-        std::cout << "Test Insert() and Find() failed.\n";
-    }
-    if (testBeginAndEnd()) {
-        std::cout << "Test Begin() and End() passed\n";
-    } else {
-        std::cout << "Test Begin() and End() failed.\n";
-    }
-    if (testUpperBound()) {
-        std::cout << "Test Upper_Bound() passed\n";
-    } else {
-        std::cout << "Test Upper_Bound() failed.\n";
-    }
-    if (testIncrementInterator()) {
-        std::cout << "Test testIncrementInterator() passed\n";
-    } else {
-        std::cout << "Test testIncrementInterator() failed.\n";
-    }
-    if (testErase()) {
-        std::cout << "Test testErase() passed\n";
-    } else {
-        std::cout << "Test testErase() failed.\n";
-    }
-    if (testLowerBound()) {
-        std::cout << "Test testLowerBound() passed\n";
-    } else {
-        std::cout << "Test testLowerBound0() failed.\n";
-    }
-    if (testClear()) {
-        std::cout << "Test testClear() passed\n";
-    } else {
-        std::cout << "Test testClear() failed.\n";
-    }
-
-
-    return 0;
 }
